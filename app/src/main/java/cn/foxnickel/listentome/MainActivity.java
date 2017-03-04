0adf155ac640122964a7c925fcdcd7daf7e5f061
NickelFox <49366499@qq.com>
2017-03-04 13:46:58 +0800

    新增了SearchView（社区页面），更改ToolBar（点击不同的模块显示不同的标题）

--- app/src/main/java/cn/foxnickel/listentome/MainActivity.java
+++ app/src/main/java/cn/foxnickel/listentome/MainActivity.java
@@ -3,7 +3,9 @@ package cn.foxnickel.listentome;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
@@ -13,6 +15,8 @@ import cn.foxnickel.listentome.fragment.ProfileFragmet;
 import cn.foxnickel.listentome.fragment.SocialFragmet;
 import cn.foxnickel.listentome.fragment.SpeechFragmet;
 
+import static android.view.View.GONE;
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener{
 
     private BottomNavigationBar bottomNavigationBar;
@@ -21,19 +25,27 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
     private SocialFragmet socialFragmet;
     private ProfileFragmet profileFragmet;
     private Toolbar toolbar;
+    private SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //toolbar.setTitle(getString(R.string.app_name));
-        setSupportActionBar(toolbar);
+        initToolBar();
         initBottomNavBar();
-
         initTab();
         bottomNavigationBar.setTabSelectedListener(this);
     }
 
+    /*初始化ToolBar*/
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        searchView = (SearchView) findViewById(R.id.search);
+        toolbar.setTitle("听力频道");//进入app时标题显示听力频道
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        setSupportActionBar(toolbar);
+    }
+
+    /*初始化底部导航栏*/
     private void initBottomNavBar(){
         bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_nav_bar);
         bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
@@ -59,6 +71,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
                 .initialise();//初始化
     }
 
+    /*初始化底部导航栏的默认选中Tab*/
     private void initTab(){
         listenFragmet = new ListenFragmet();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -66,29 +79,41 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
         transaction.commit();
     }
 
+    /*Tab被选中时的监听事件*/
     @Override
     public void onTabSelected(int position) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         switch (position){
             case 0:
+                toolbar.setVisibility(View.VISIBLE);
+                toolbar.setTitle("听力频道");//点击听力模块时标题显示听力频道
+                searchView.setVisibility(GONE);
                 if(listenFragmet == null){
                     listenFragmet = new ListenFragmet();
                 }
                 transaction.replace(R.id.content_main,listenFragmet);
                 break;
             case 1:
+                toolbar.setVisibility(View.VISIBLE);
+                toolbar.setTitle("口语天地");//点击口语模块时标题显示口语天地
+                searchView.setVisibility(GONE);
                 if(speechFragmet == null){
                     speechFragmet = new SpeechFragmet();
                 }
                 transaction.replace(R.id.content_main,speechFragmet);
                 break;
             case 2:
+                toolbar.setVisibility(GONE);
+                searchView.setVisibility(View.VISIBLE);
                 if(socialFragmet == null){
                     socialFragmet = new SocialFragmet();
                 }
                 transaction.replace(R.id.content_main,socialFragmet);
                 break;
             case 3:
+                toolbar.setVisibility(View.VISIBLE);
+                toolbar.setTitle("个人中心");//点击我的时标题显示个人中心
+                searchView.setVisibility(GONE);
                 if(profileFragmet == null){
                     profileFragmet = new ProfileFragmet();
                 }
