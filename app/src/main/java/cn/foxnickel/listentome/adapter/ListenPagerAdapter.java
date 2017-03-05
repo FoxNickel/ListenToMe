37aab03bc000c9f73a53dfbb7261fe50db78e427
NickelFox <49366499@qq.com>
2017-03-05 19:49:51 +0800

    新建了ListenFragment的布局（subFragment形式），出现了选择其他模块之后ListenFragment子布局会没掉的bug

---
+++ app/src/main/java/cn/foxnickel/listentome/adapter/ListenPagerAdapter.java
@@ -0,0 +1,41 @@
+package cn.foxnickel.listentome.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/5.
+ */
+
+public class ListenPagerAdapter extends FragmentPagerAdapter {
+
+    private final int PAGER_COUNT = 2;
+    private Context mContext;
+    private List<Fragment> mFragmentList;
+    private String[] mPagerTitles = {"练习", "考试"};
+
+    public ListenPagerAdapter(FragmentManager fm, Context context, List<Fragment> fragmentList) {
+        super(fm);
+        mContext = context;
+        mFragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGER_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mPagerTitles[position];
+    }
+}
--- app/src/main/java/cn/foxnickel/listentome/fragment/ListenFragmet.java
+++ app/src/main/java/cn/foxnickel/listentome/fragment/ListenFragmet.java
@@ -2,13 +2,21 @@ package cn.foxnickel.listentome.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.bingoogolapple.bgabanner.BGABanner;
 import cn.foxnickel.listentome.R;
+import cn.foxnickel.listentome.adapter.ListenPagerAdapter;
+import cn.foxnickel.listentome.fragment.subfragment.ExamFragment;
+import cn.foxnickel.listentome.fragment.subfragment.ExerciseFragment;
 
 /**
  * Created by Administrator on 2017/3/3.
@@ -17,12 +25,27 @@ import cn.foxnickel.listentome.R;
 public class ListenFragmet extends Fragment {
     private View mRootView;
     private BGABanner mBanneer;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private List<Fragment> mFragmentList;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        /*根布局*/
         mRootView = inflater.inflate(R.layout.fragment_listen, container, false);
+        /*图片轮播*/
         mBanneer = (BGABanner) mRootView.findViewById(R.id.pic_banner);
-        mBanneer.setData(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3);
+        mBanneer.setData(R.drawable.pic6, R.drawable.pic6, R.drawable.pic6);
+
+        mFragmentList = new ArrayList<>();
+        mFragmentList.add(new ExerciseFragment());
+        mFragmentList.add(new ExamFragment());
+
+        ListenPagerAdapter listenPagerAdapter = new ListenPagerAdapter(getFragmentManager(), getContext(), mFragmentList);
+        mTabLayout = (TabLayout) mRootView.findViewById(R.id.tab_layout);
+        mViewPager = (ViewPager) mRootView.findViewById(R.id.view_pager);
+        mViewPager.setAdapter(listenPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
         return mRootView;
     }
 
