0592b316d122225dd21317cc2229deac5012366f
NickelFox <49366499@qq.com>
2017-03-05 20:13:08 +0800

    新建了ListenFragment的布局（PagerAdapter+子View（未完善））

--- app/src/main/java/cn/foxnickel/listentome/fragment/ListenFragmet.java
+++ app/src/main/java/cn/foxnickel/listentome/fragment/ListenFragmet.java
@@ -2,13 +2,19 @@ package cn.foxnickel.listentome.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.bingoogolapple.bgabanner.BGABanner;
 import cn.foxnickel.listentome.R;
+import cn.foxnickel.listentome.fragment.adapter.ListenPagerAdapter;
 
 /**
  * Created by Administrator on 2017/3/3.
@@ -17,12 +23,32 @@ import cn.foxnickel.listentome.R;
 public class ListenFragmet extends Fragment {
     private View mRootView;
     private BGABanner mBanneer;
+    private View mExerciseView, mExamView;
+    private List<View> mViewList;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_listen, container, false);
         mBanneer = (BGABanner) mRootView.findViewById(R.id.pic_banner);
-        mBanneer.setData(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3);
+        mBanneer.setData(R.drawable.pic4, R.drawable.pic4, R.drawable.pic4);
+
+        mViewList = new ArrayList<>();
+        mExerciseView = inflater.inflate(R.layout.exercise, container, false);
+        mExamView = inflater.inflate(R.layout.exam, container, false);
+
+        mViewList.add(mExerciseView);
+        mViewList.add(mExamView);
+
+        ListenPagerAdapter listenPagerAdapter = new ListenPagerAdapter(mViewList);
+
+        mViewPager = (ViewPager) mRootView.findViewById(R.id.viewPager);
+        mViewPager.setAdapter(listenPagerAdapter);
+
+        mTabLayout = (TabLayout) mRootView.findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
         return mRootView;
     }
 
---
+++ app/src/main/java/cn/foxnickel/listentome/fragment/adapter/ListenPagerAdapter.java
@@ -0,0 +1,43 @@
+package cn.foxnickel.listentome.fragment.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/5.
+ */
+
+public class ListenPagerAdapter extends PagerAdapter {
+
+    private final int PAGER_COUNT = 2;
+    private List<View> mViewList;
+    private String[] mPageTitles = {"练习", "考试"};
+
+    public ListenPagerAdapter(List<View> viewList) {
+        mViewList = viewList;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGER_COUNT;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(mViewList.get(position));
+        return mViewList.get(position);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mPageTitles[position];
+    }
+}
