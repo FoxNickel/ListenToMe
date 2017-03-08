b5020a06fb0bcec9dcdb39f7faf495e9b3e24b66
NickelFox <49366499@qq.com>
2017-03-08 23:40:24 +0800

    个人中心的TooBar上增加了settings按钮

--- app/src/main/java/cn/foxnickel/listentome/fragment/ProfileFragmet.java
+++ app/src/main/java/cn/foxnickel/listentome/fragment/ProfileFragmet.java
@@ -3,6 +3,7 @@ package cn.foxnickel.listentome.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +16,20 @@ import cn.foxnickel.listentome.R;
 
 public class ProfileFragmet extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     private View mRootView;
+    private Toolbar mToolbar;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        mToolbar = (Toolbar) mRootView.findViewById(R.id.toolbar);
+        mToolbar.inflateMenu(R.menu.action_settings);
         return mRootView;
     }
 }
