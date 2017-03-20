546b82bfeb98f6c059f7b417348966e192ebaa46
Night <841570240@qq.com>
2017-03-20 19:37:46 +0800

    OKHttp工具类编写

---
+++ app/src/main/java/cn/foxnickel/listentome/utils/OkHttpManager.java
@@ -0,0 +1,196 @@
+package cn.foxnickel.listentome.utils;
+
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Night on 2017/3/20.
+ * Desc:
+ */
+
+public class OkHttpManager {
+    private static final OkHttpClient mOkHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS).readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS).build();
+    private Handler handler = new Handler(Looper.getMainLooper());
+
+    /**
+     * 该不会开启异步线程。
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+
+    public static Response execute(Request request) throws IOException {
+        return mOkHttpClient.newCall(request).execute();
+    }
+
+    /**
+     * 开启异步线程访问网络
+     *
+     * @param request
+     * @param responseCallback
+     */
+    public static void enqueue(Request request, Callback responseCallback) {
+        mOkHttpClient.newCall(request).enqueue(responseCallback);
+    }
+
+    /**
+     * 开启异步线程访问网络, 且不在意返回结果（实现空callback）
+     *
+     * @param request
+     */
+    public static void enqueue(Request request) {
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+    }
+
+    public static String getStringFromServer(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        Response response = execute(request);
+        if (response.isSuccessful()) {
+            String responseUrl = response.body().string();
+            return responseUrl;
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
+    }
+
+    private static final String CHARSET_NAME = "UTF-8";
+
+
+    /**
+     * 异步请求,请求返回Json字符串
+     *
+     * @param url
+     * @param callback
+     */
+    public void asyncJsonStringByURL(String url, final Fun1 callback) {
+        final Request request = new Request.Builder().url(url).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            //enqueue是调用了一个入队的方法
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response != null && response.isSuccessful()) {
+                    onSuccessJsonStringMethod(response.body().string(), callback);
+                }
+            }
+        });
+
+    }
+
+    /**
+     * 异步请求，请求返回Json对象
+     *
+     * @param url
+     * @param callback
+     */
+
+    public void asyncJsonObjectByUrl(String url, final Fun4 callback) {
+        final Request request = new Request.Builder().url(url).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response != null && response.isSuccessful()) {
+                    onSuccessJsonObjectMethod(response.body().string(), callback);
+                }
+            }
+        });
+
+    }
+
+    //回调
+    public interface Fun1 {
+        void onResponse(String result);
+    }
+
+    interface Fun2 {
+        void onResponse(byte[] result);
+    }
+
+    public interface Fun3 {
+        void onResponse(Bitmap bitmap);
+    }
+
+    public interface Fun4 {
+        void onResponse(JSONObject jsonObject);
+    }
+
+    /**
+     * 请求返回的是JSON字符串
+     *
+     * @param jsonValue
+     * @param callBack
+     */
+    private void onSuccessJsonStringMethod(final String jsonValue, final Fun1 callBack) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (callBack != null) {
+                    try {
+                        callBack.onResponse(jsonValue);
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        });
+
+    }
+
+    /**
+     * 请求返回相应结果的是Json对象
+     *
+     * @param jsonValue
+     * @param callBack
+     */
+    private void onSuccessJsonObjectMethod(final String jsonValue, final Fun4 callBack) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (callBack != null) {
+                    try {
+                        callBack.onResponse(new JSONObject(jsonValue));
+                    } catch (JSONException e) {
+
+                    }
+                }
+            }
+        });
+    }
+
+}
