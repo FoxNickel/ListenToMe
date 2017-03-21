e1107bd9adc5c3b695dad381ddc8b41af5c76d1c
NickelFox <49366499@qq.com>
2017-03-21 23:38:54 +0800

    密码最大长度改为16位

--- app/src/main/java/cn/foxnickel/listentome/LoginActivity.java
+++ app/src/main/java/cn/foxnickel/listentome/LoginActivity.java
@@ -8,7 +8,6 @@ import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -16,9 +15,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -32,16 +28,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.blankj.utilcode.utils.ToastUtils;
-
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.foxnickel.listentome.utils.AESUtils;
-
-import static android.Manifest.permission.READ_CONTACTS;
-import static android.os.Build.VERSION_CODES.M;
 
 /**
  * A login screen that offers login via email/password.
@@ -198,7 +189,7 @@ public class LoginActivity extends BaseActivity implements LoaderCallbacks<Curso
     }
 
     private boolean isPasswordValid(String password) {
-        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$";
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$";
         return password.matches(regex);
     }
 
