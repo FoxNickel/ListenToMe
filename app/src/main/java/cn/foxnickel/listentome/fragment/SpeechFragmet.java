2c463a2496ecccd0178724e36edfd849ee2ecf63
NickelFox <49366499@qq.com>
2017-03-22 21:58:10 +0800

    更改了密码不符合规则和账号错误时的提示，新建了口语界面

--- app/src/main/java/cn/foxnickel/listentome/fragment/SpeechFragmet.java
+++ app/src/main/java/cn/foxnickel/listentome/fragment/SpeechFragmet.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import cn.bingoogolapple.bgabanner.BGABanner;
 import cn.foxnickel.listentome.R;
 
 /**
@@ -16,11 +17,25 @@ import cn.foxnickel.listentome.R;
 public class SpeechFragmet extends Fragment {
 
     private View mRootView;
+    private BGABanner mBanner;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_speech, container, false);
-        ;
+        initPicBanner();
         return mRootView;
     }
+
+    /*初始化图片Banner*/
+    private void initPicBanner() {
+        new Runnable() {
+            @Override
+            public void run() {
+                mBanner = (BGABanner) mRootView.findViewById(R.id.pic_banner);
+                mBanner.setData(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3);
+            }
+        }.run();
+
+    }
+
 }
