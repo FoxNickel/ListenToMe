2ba9e0bd82cc38c448c3855800f1fede20b67891
Night <841570240@qq.com>
2017-03-24 19:54:08 +0800

    听力考试界面新建

---
+++ app/src/main/java/cn/foxnickel/listentome/ListenExamActivity.java
@@ -0,0 +1,40 @@
+package cn.foxnickel.listentome;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.foxnickel.listentome.adapter.ListenExamAdater;
+import cn.foxnickel.listentome.bean.HearingIssueBean;
+import cn.foxnickel.listentome.bean.ListenExamBean;
+import cn.foxnickel.listentome.bean.SocialBean;
+
+/**
+ * Created by Night on 2017/3/24.
+ * Desc:
+ */
+
+public class ListenExamActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ListenExamAdater mExamAdater;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen_exam);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_question);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        ArrayList<HearingIssueBean> list = new ArrayList<>();
+        list.add(new HearingIssueBean(0, 0, 0, "sdfsdfsdf", "sdsdfsdf", "sdfsdfsdfsdf", 0, 0, 0));
+        mExamAdater = new ListenExamAdater(list, this);
+        mRecyclerView.setAdapter(mExamAdater);
+    }
+}
---
+++ app/src/main/java/cn/foxnickel/listentome/adapter/ListenExamAdater.java
@@ -0,0 +1,101 @@
+/**
+ * Created by Night on 2017/3/23.
+ * Desc:
+ */
+package cn.foxnickel.listentome.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.foxnickel.listentome.R;
+import cn.foxnickel.listentome.bean.HearingIssueBean;
+
+/**
+ * @author Night
+ * @since 2017-03-23
+ */
+public class ListenExamAdater extends RecyclerView.Adapter<ListenExamAdater.ViewHolder> {
+    private Context mContext;
+    private List<HearingIssueBean> mList;
+
+    public ListenExamAdater(List<HearingIssueBean> items, Context context) {
+        this.mList = items;
+        this.mContext = context;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View wrapper = LayoutInflater.from(mContext).inflate(R.layout.listen_exam_item, parent, false);
+        return new ViewHolder(wrapper, (TextView) wrapper.findViewById(R.id.tv_question_num),
+                (ImageView) wrapper.findViewById(R.id.lv_answer_a),
+                (ImageView) wrapper.findViewById(R.id.im_answer_bg_a),
+                (ImageView) wrapper.findViewById(R.id.lv_answer_b),
+                (ImageView) wrapper.findViewById(R.id.im_answer_bg_b),
+                (ImageView) wrapper.findViewById(R.id.lv_answer_c),
+                (ImageView) wrapper.findViewById(R.id.im_answer_bg_c),
+                (ImageView) wrapper.findViewById(R.id.lv_answer_d),
+                (ImageView) wrapper.findViewById(R.id.im_answer_bg_d));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        HearingIssueBean hearingIssueBean = mList.get(position);
+        holder.question.setText(hearingIssueBean.getHIContent());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if (mList == null) {
+            return 0;
+        }
+        return mList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public View rootView;
+        private TextView question;
+        private ImageView answer_a, answer_bg_a, answer_b, answer_bg_b, answer_c, answer_bg_c, answer_d, answer_bg_d;
+
+        public ViewHolder(View rootView, TextView question, ImageView a_a, ImageView a_b_a, ImageView a_b, ImageView a_b_b, ImageView a_c, ImageView a_b_c, ImageView a_d, ImageView a_b_d) {
+            super(rootView);
+            this.rootView = rootView;
+            this.question = question;
+            answer_a = a_a;
+            answer_bg_a = a_b_a;
+            answer_b = a_b;
+            answer_bg_b = a_b_b;
+            answer_c = a_c;
+            answer_bg_c = a_b_c;
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.lv_answer_a:
+                    answer_bg_a.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.lv_answer_b:
+                    answer_bg_b.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.lv_answer_c:
+                    answer_bg_c.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.lv_answer_d:
+                    answer_bg_d.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
