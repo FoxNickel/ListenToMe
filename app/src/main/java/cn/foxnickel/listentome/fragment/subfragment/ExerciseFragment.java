c327687e15fcd5b5e3238ca45510dbcdf3db9152
NickelFox <49366499@qq.com>
2017-03-29 19:14:33 +0800

    听力练习模式界面更改，考试模式图标更改

--- app/src/main/java/cn/foxnickel/listentome/fragment/subfragment/ExerciseFragment.java
+++ app/src/main/java/cn/foxnickel/listentome/fragment/subfragment/ExerciseFragment.java
@@ -1,14 +1,16 @@
 package cn.foxnickel.listentome.fragment.subfragment;
 
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import com.blankj.utilcode.utils.ImageUtils;
 
 import cn.foxnickel.listentome.R;
 
@@ -18,8 +20,9 @@ import cn.foxnickel.listentome.R;
 public class ExerciseFragment extends Fragment implements View.OnClickListener {
 
     private View mRootView;
-    private Button mEasy, mMiddle, mHard, mStartExercise;
-    private ImageView mModeImage;
+    private ImageView mEasyImage, mMiddleImage, mHardImage;
+    private CardView mEasyCard, mMiddleCard, mHardCard;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,18 +33,18 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener {
     }
 
     private void setListener() {
-        mEasy.setOnClickListener(this);
-        mMiddle.setOnClickListener(this);
-        mHard.setOnClickListener(this);
-        mStartExercise.setOnClickListener(this);
+
     }
 
     private void initView() {
-        mEasy = (Button) mRootView.findViewById(R.id.exercise_easy);
-        mMiddle = (Button) mRootView.findViewById(R.id.exercise_middle);
-        mHard = (Button) mRootView.findViewById(R.id.exercise_hard);
-        mModeImage = (ImageView) mRootView.findViewById(R.id.mode_image);
-        mStartExercise = (Button) mRootView.findViewById(R.id.start_exercise);
+        Bitmap levelPic = ImageUtils.getBitmap(getResources(), R.drawable.pic7);
+        levelPic = ImageUtils.toRound(levelPic);
+        mEasyImage = (ImageView) mRootView.findViewById(R.id.easy_image);
+        mMiddleImage = (ImageView) mRootView.findViewById(R.id.middle_image);
+        mHardImage = (ImageView) mRootView.findViewById(R.id.hard_image);
+        mEasyImage.setImageBitmap(levelPic);
+        mMiddleImage.setImageBitmap(levelPic);
+        mHardImage.setImageBitmap(levelPic);
     }
 
 
@@ -49,27 +52,7 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         int id = v.getId();
         switch (id) {
-            case R.id.exercise_easy:
-                mModeImage.setImageResource(R.drawable.exercise_easy);
-                mEasy.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                mMiddle.setBackgroundColor(getResources().getColor(android.R.color.white));
-                mHard.setBackgroundColor(getResources().getColor(android.R.color.white));
-                break;
-            case R.id.exercise_middle:
-                mModeImage.setImageResource(R.drawable.exercise_middle);
-                mMiddle.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                mEasy.setBackgroundColor(getResources().getColor(android.R.color.white));
-                mHard.setBackgroundColor(getResources().getColor(android.R.color.white));
-                break;
-            case R.id.exercise_hard:
-                mModeImage.setImageResource(R.drawable.exercise_hard);
-                mHard.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                mMiddle.setBackgroundColor(getResources().getColor(android.R.color.white));
-                mEasy.setBackgroundColor(getResources().getColor(android.R.color.white));
-                break;
-            case R.id.start_exercise:
-                Toast.makeText(getActivity(), "开始练习", Toast.LENGTH_SHORT).show();
-                break;
+
         }
     }
 }
